8425981fb05712801b959187398f310d0b830cfd
Adriana Batista <batistasilvaadriana@gmail.com>
2019-04-24 11:44:17 -0300

    novo commit do exer_js

---
+++ exer_js_01/javascript.js
@@ -0,0 +1,15 @@
+// isso é um comentario de linha 
+/* isso é um comentario de bloco  */
+
+// Declarando variáveis
+let nome;
+let apelido;
+
+// Atribuindo valores
+nome = "adriana batista";
+apelido = "drika";
+
+console.log(nome);
+console.log(apelido);
+
+
